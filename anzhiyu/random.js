a7000eaa7988483521e72f9935269e35276af080
AnZhiJJ <2813136077@qq.com>
2023-05-13 19:12:31 +0800

    Site updated: 2023-05-13 19:12:30

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["techdoc1.html","techdoc2.html","pythonbasic1.html","C9.html","pythonbasic2.html","pythonbasic3.html","pythonbasic5.html","C8.html","pythonbasic4.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["C9.html","techdoc1.html","C8.html","techdoc2.html","pythonbasic3.html","pythonbasic2.html","pythonbasic5.html","pythonbasic4.html","pythonbasic1.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
