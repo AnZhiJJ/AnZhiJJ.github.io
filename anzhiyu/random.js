79fa99622506c8487699582f6869e9fff6acae91
AnZhiJJ <2813136077@qq.com>
2023-05-14 10:29:22 +0800

    Site updated: 2023-05-14 10:29:21

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["techdoc1.html","C9.html","pythonbasic1.html","C8.html","techdoc2.html","pythonbasic3.html","pythonbasic2.html","pythonbasic4.html","pythonbasic5.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["C9.html","C8.html","techdoc2.html","techdoc1.html","pythonbasic2.html","pythonbasic1.html","pythonbasic3.html","pythonbasic4.html","pythonbasic5.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
